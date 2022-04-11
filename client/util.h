7ea23b9842d0e2753aee381d9eac789710c863a1
Muhammad Abdullah <abd880@mit.edu>
2022-04-10 20:57:42 -0400

    updated specs

--- client/util.h
+++ client/util.h
@@ -6,18 +6,17 @@
 #define DEBOUNCE_DURATION 10
 #define JOYSTICK_DEADZONE 200
 
-enum button_state
-{
-    S0,
-    S1,
-    S2,
-    S3,
-    S4
-};
-
 /**
- * @brief Button class
- * @param p pin number, itialize it in setup()
+ * @brief Button class, just call update() to read the button state
+ *   the result is stored in the flag variable.
+ *  flag = 0 means no reading
+ *  flag = 1 means a short press happened
+ *  flag = 2 means a long press happened (>1 second)
+ *
+ *  The button is debounced by a 10ms delay.
+ *  Most of the time, you only care to check if flag > 0.
+ *
+ * @param p pin number, itialize its mode in setup()
  */
 class Button
 {
@@ -29,11 +28,41 @@ class Button
     int button_pressed;
     button_state state;
     void read();
-    int update();
 
 public:
+    int update();
     Button(int p);
     int flag;
+};
+
+/**
+ * @brief a class for interfacing with a joystick on an arduino, need to initialize the pins yourself
+ *        outside the constructor
+ *
+ * @param VRx the pin the joystick is connected to for the x-axis
+ * @param VRy the pin the joystick is connected to for the y-axis
+ * @param Sw the pin the joystick is connected to for the button
+ * @param button_mode if 1, button will return 1 during rising edge, -1 during falling edge
+ *                    if 0, button is a normal button
+ */
+class Joystick
+{
+    int VRx;
+    int VRy;
+    int Sw;
+    int VRx_val;
+    int VRy_val;
+    int Sw_val;
+    int button_mode;
+    int previous_button_pressed = 0;
+
+    Button button;
+    void read();
+
+public:
+    Joystick(int VRx, int VRy, int Sw, int button_mode);
+    joystick_direction update();
+    joystick_direction direction;
 };
 
 enum joystick_direction
@@ -49,33 +78,13 @@ enum joystick_direction
     DOWN_RIGHT
 };
 
-/**
- * @brief a class for interfacing with a joystick on an arduino, need to initialize the pins yourself
- *        outside the constructor
- *
- * @param VRx the pin the joystick is connected to for the x-axis
- * @param VRy the pin the joystick is connected to for the y-axis
- * @param Sw the pin the joystick is connected to for the button
- * @param button_mode if 1, button will return 1 during rising edge, -1 during falling edge
- *                    if 0, button is a normal button
- */
-class Joystick
+enum button_state
 {
-    Joystick(int VRx, int VRy, int Sw, int button_mode);
-    int VRx;
-    int VRy;
-    int Sw;
-    int VRx_val;
-    int VRy_val;
-    int Sw_val;
-    int button_mode;
-    int previous_button_pressed = 0;
-
-    Button button;
-    void read();
-    joystick_direction update();
-
-    joystick_direction direction;
+    S0,
+    S1,
+    S2,
+    S3,
+    S4
 };
 
 #endif
