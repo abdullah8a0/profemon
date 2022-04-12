72f3c8d6ce241a4ad06c899b850f8cf94bc46a6a
Muhammad Abdullah <abd880@mit.edu>
2022-04-11 21:31:10 -0400

    added as comment

--- client/client.ino
+++ client/client.ino
@@ -137,3 +137,50 @@ void catch_request(char *uid, char *RESPONSE)
         offset += sprintf(request + offset, "%s\r\n", request_body);
         do_http_request("608dev-2.net", request, RESPONSE, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT);
     }
+}
+/*
+#include "util.h"
+int VRx = 5; // 5 pin
+int VRy = 1; // 1 pin
+int Sw = 12; // 12 pin
+Joystick joystick(VRx, VRy, Sw, 0);
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("setup");
+    // initialize the joystick
+    pinMode(VRx, INPUT);
+    pinMode(VRy, INPUT);
+    pinMode(Sw, INPUT_PULLUP);
+    analogReadResolution(10);
+}
+
+// print the direction of the joystick
+void loop()
+{
+    Serial.println("here 0");
+    joystick_direction d = joystick.update();
+    // Serial.print("direction: ");
+    switch (d)
+    {
+    case NONE:
+        // Serial.println("NONE");
+        break;
+    case UP:
+        Serial.println("UP");
+        break;
+    case DOWN:
+        Serial.println("DOWN");
+        break;
+    case LEFT:
+        Serial.println("LEFT");
+        break;
+    case RIGHT:
+        Serial.println("RIGHT");
+        break;
+    }
+    // Serial.print("Sw: ");
+    // Serial.println(joystick.Sw_val);
+    // delay(500);
+}
+*/
