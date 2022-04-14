636955e456f1bb85437fc535842229eef006bc94
Heidi Lei <hlei1218@gmail.com>
2022-04-14 12:07:29 -0400

    added jpg2bin_small for smaller thumbnails displayed presumably on the battle stage

--- images/jpg2bin.py
+++ images/jpg2bin.py
@@ -1,7 +1,18 @@
+from PIL import Image
+import io
+
 def jpg2bin(jpg_file):
     with open(jpg_file, 'rb') as f:
         return f.read()
 
+def jpg2bin_small(jpg_file, width=32, height=40):
+    img = Image.open(jpg_file)
+    img.thumbnail((width, height))
+    output = io.BytesIO()
+    img.save(output, format='JPEG')
+    return output.getvalue()
+
 if __name__ == '__main__':
     file = 'JoeSteinmeyer.jpg'
-    print(jpg2bin(file))
+    print(len(jpg2bin(file)))
+    print(len(jpg2bin_small(file)))
